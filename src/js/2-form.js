d2b81a79415b4dc6b0fc513716f570e4292b1810
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2024-04-25 10:54:49 +0100

    hw_4

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,19 +3,15 @@ const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 const localStorageKey = 'feedback-form-state';
 
-let localStorageData = JSON.parse(localStorage.getItem(localStorageKey))
-  ? JSON.parse(localStorage.getItem(localStorageKey))
-  : {};
+let localStorageData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
 
-email.value = localStorageData.hasOwnProperty('email')
-  ? localStorageData.email
-  : '';
-message.value = localStorageData.hasOwnProperty('message')
-  ? localStorageData.message
-  : '';
+const setFormValuesFromLocalStorage = () => {
+  email.value = localStorageData.email ? localStorageData.email.trim() : '';
+  message.value = localStorageData.message ? localStorageData.message.trim() : '';
+};
 
 const handleInput = (event, field) => {
-  const value = event.target.value;
+  const value = event.target.value.trim();
   localStorageData = { ...localStorageData, [field]: value };
   localStorage.setItem(localStorageKey, JSON.stringify(localStorageData));
 };
@@ -24,15 +20,20 @@ const handleSubmit = event => {
   event.preventDefault();
   const form = event.target;
 
-  if (email.value.length === 0 || message.value.length === 0) {
+  if (email.value.trim() === '' || message.value.trim() === '') {
     alert('The email and message fields must be filled in');
-    throw new Error('The email and message fields must be filled in');
+    return;
   }
 
-  console.log({ email: email.value, message: message.value });
-  localStorage.clear();
+  console.log({ email: email.value.trim(), message: message.value.trim() });
+  localStorageData = {};
+  localStorage.setItem(localStorageKey, JSON.stringify(localStorageData));
   form.reset();
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+  setFormValuesFromLocalStorage(); // Встановлення значень форми при завантаженні сторінки
+});
 
 email.addEventListener('input', event => handleInput(event, 'email'));
 message.addEventListener('input', event => handleInput(event, 'message'));
